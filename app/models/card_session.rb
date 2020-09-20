0fcbc1342479d983cbaa6fc8a9d67161600857cc
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2020-09-20 17:55:30 +0200

    build card session

--- app/models/card_session.rb
+++ app/models/card_session.rb
@@ -1,3 +1,22 @@
+# frozen_string_literal: true
+
 class CardSession < ApplicationRecord
   belongs_to :user, optional: true
+  has_many :card_session_problems, inverse_of: :card_session
+  has_many :problems, through: :card_session_problems
+
+  def self.build_addition(range:)
+    card_session = CardSession.new
+    range_as_array = get_range_as_array(range)
+    card_session.problems = range_as_array.map do |number|
+      (0..10).map do |number_2|
+        Addition.find_or_create_by!(number_1: number, number_2: (1..10).to_a.sample, hole_position: [nil, 1, 2].sample)
+      end
+    end.flatten
+    card_session
+  end
+
+  def self.get_range_as_array(range)
+    [*range]
+  end
 end
--- spec/models/card_session_spec.rb
+++ spec/models/card_session_spec.rb
@@ -1,5 +1,24 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe CardSession, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.build_addition' do
+    subject(:card_session) { CardSession.build_addition(range: range) }
+    # CardSession.build_addition([4, 5])
+    context 'with single number as range' do
+      let(:range) { 4 }
+      it { expect(card_session.problems.size).to eq(11) }
+    end
+
+    context 'with array of number as range' do
+      let(:range) { [4, 5] }
+      it { expect(card_session.problems.size).to eq(22) }
+    end
+
+    context 'with range as range' do
+      let(:range) { (4..6) }
+      it { expect(card_session.problems.size).to eq(33) }
+    end
+  end
 end
