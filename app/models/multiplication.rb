0ba6d151a8a708c91fb08e932a5e26824cf9272a
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2021-12-28 11:04:42 +0100

    2021d24.1

--- app/models/multiplication.rb
+++ app/models/multiplication.rb
@@ -3,22 +3,22 @@
 class Multiplication < Problem
   def self.random(_user)
     number2_freq_array = [
-      [1, 0],
-      [1, 1],
-      [4, 2],
-      [4, 3],
-      [4, 4],
-      [4, 5],
+      # [1, 0],
+      # [1, 1],
+      # [4, 2],
+      # [4, 3],
+      # [4, 4],
+      # [4, 5],
       [6, 6],
       [6, 7],
       [6, 8],
-      [6, 9],
-      [6, 10],
-      [2, 11],
-      [2, 12]
+      # [6, 9],
+      # [6, 10],
+      # [2, 11],
+      # [2, 12]
     ]
 
-    number_1 ||= (2..5).to_a.sample
+    number_1 ||= (2..9).to_a.sample
     number_2 ||= number2_freq_array.reduce([]) { |array, frequence| array += Array.new(frequence.first, frequence.second) }.sample
 
     if [true, false].sample
