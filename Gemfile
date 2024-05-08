883f755227f4857122a4794f78509a12d622d70e
Benoit Lafontaine <benoit.lafontaine@clickandboat.com>
2024-05-08 19:36:34 +0200

    Upgrade rails

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '3.3.1'
 
-gem 'rails', '~> 7.0.8'
+gem 'rails', '~> 7.1'
 
 gem 'bootsnap', require: false
 gem 'devise'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_231_203_230_226) do
+ActiveRecord::Schema[7.1].define(version: 20_240_508_173_150) do
+  create_schema 'math_trainer'
+
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -19,8 +21,8 @@ ActiveRecord::Schema[7.0].define(version: 20_231_203_230_226) do
     t.bigint 'fight_id', null: false
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
-    t.index ['answer_id'], name: 'index_math_trainer_answer_fights_on_answer_id'
-    t.index ['fight_id'], name: 'index_math_trainer_answer_fights_on_fight_id'
+    t.index ['answer_id'], name: 'index_answer_fights_on_answer_id'
+    t.index ['fight_id'], name: 'index_answer_fights_on_fight_id'
   end
 
   create_table 'answers', force: :cascade do |t|
@@ -31,9 +33,9 @@ ActiveRecord::Schema[7.0].define(version: 20_231_203_230_226) do
     t.datetime 'updated_at', null: false
     t.boolean 'correct'
     t.bigint 'card_session_id'
-    t.index ['card_session_id'], name: 'index_math_trainer_answers_on_card_session_id'
-    t.index ['problem_id'], name: 'index_math_trainer_answers_on_problem_id'
-    t.index ['user_id'], name: 'index_math_trainer_answers_on_user_id'
+    t.index ['card_session_id'], name: 'index_answers_on_card_session_id'
+    t.index ['problem_id'], name: 'index_answers_on_problem_id'
+    t.index ['user_id'], name: 'index_answers_on_user_id'
   end
 
   create_table 'answers_sessions', force: :cascade do |t|
@@ -41,8 +43,8 @@ ActiveRecord::Schema[7.0].define(version: 20_231_203_230_226) do
     t.bigint 'time_session_id', null: false
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
-    t.index ['answer_id'], name: 'index_math_trainer_answers_sessions_on_answer_id'
-    t.index ['time_session_id'], name: 'index_math_trainer_answers_sessions_on_time_session_id'
+    t.index ['answer_id'], name: 'index_answers_sessions_on_answer_id'
+    t.index ['time_session_id'], name: 'index_answers_sessions_on_time_session_id'
   end
 
   create_table 'card_session_problems', force: :cascade do |t|
@@ -50,8 +52,8 @@ ActiveRecord::Schema[7.0].define(version: 20_231_203_230_226) do
     t.bigint 'card_session_id', null: false
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
-    t.index ['card_session_id'], name: 'index_math_trainer_card_session_problems_on_card_session_id'
-    t.index ['problem_id'], name: 'index_math_trainer_card_session_problems_on_problem_id'
+    t.index ['card_session_id'], name: 'index_card_session_problems_on_card_session_id'
+    t.index ['problem_id'], name: 'index_card_session_problems_on_problem_id'
   end
 
   create_table 'card_sessions', force: :cascade do |t|
@@ -59,7 +61,7 @@ ActiveRecord::Schema[7.0].define(version: 20_231_203_230_226) do
     t.bigint 'user_id'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_math_trainer_card_sessions_on_user_id'
+    t.index ['user_id'], name: 'index_card_sessions_on_user_id'
   end
 
   create_table 'fight_opponents', force: :cascade do |t|
@@ -81,11 +83,88 @@ ActiveRecord::Schema[7.0].define(version: 20_231_203_230_226) do
     t.string 'name'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.index ['fight_opponent_id'], name: 'index_fights_on_fight_opponent_id'
+    t.index ['user_id'], name: 'index_fights_on_user_id'
+  end
+
+  create_table 'math_trainer_answer_fights', force: :cascade do |t|
+    t.bigint 'answer_id', null: false
+    t.bigint 'fight_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['answer_id'], name: 'index_math_trainer_answer_fights_on_answer_id'
+    t.index ['fight_id'], name: 'index_math_trainer_answer_fights_on_fight_id'
+  end
+
+  create_table 'math_trainer_answers', force: :cascade do |t|
+    t.bigint 'user_id', null: false
+    t.bigint 'problem_id', null: false
+    t.string 'text'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.boolean 'correct'
+    t.bigint 'card_session_id'
+    t.index ['card_session_id'], name: 'index_math_trainer_answers_on_card_session_id'
+    t.index ['problem_id'], name: 'index_math_trainer_answers_on_problem_id'
+    t.index ['user_id'], name: 'index_math_trainer_answers_on_user_id'
+  end
+
+  create_table 'math_trainer_answers_sessions', force: :cascade do |t|
+    t.bigint 'answer_id', null: false
+    t.bigint 'time_session_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['answer_id'], name: 'index_math_trainer_answers_sessions_on_answer_id'
+    t.index ['time_session_id'], name: 'index_math_trainer_answers_sessions_on_time_session_id'
+  end
+
+  create_table 'math_trainer_ar_internal_metadata', primary_key: 'key', id: :string, force: :cascade do |t|
+    t.string 'value'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+  end
+
+  create_table 'math_trainer_card_session_problems', force: :cascade do |t|
+    t.bigint 'problem_id', null: false
+    t.bigint 'card_session_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['card_session_id'], name: 'index_math_trainer_card_session_problems_on_card_session_id'
+    t.index ['problem_id'], name: 'index_math_trainer_card_session_problems_on_problem_id'
+  end
+
+  create_table 'math_trainer_card_sessions', force: :cascade do |t|
+    t.string 'title'
+    t.bigint 'user_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_math_trainer_card_sessions_on_user_id'
+  end
+
+  create_table 'math_trainer_fight_opponents', force: :cascade do |t|
+    t.string 'name'
+    t.integer 'health'
+    t.integer 'speed'
+    t.integer 'color_rot'
+    t.jsonb 'operation_types', default: {}
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+  end
+
+  create_table 'math_trainer_fights', force: :cascade do |t|
+    t.bigint 'fight_opponent_id', null: false
+    t.bigint 'user_id', null: false
+    t.integer 'remaining_player_health'
+    t.integer 'remaining_opponent_health'
+    t.integer 'round_duration'
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
     t.index ['fight_opponent_id'], name: 'index_math_trainer_fights_on_fight_opponent_id'
     t.index ['user_id'], name: 'index_math_trainer_fights_on_user_id'
   end
 
-  create_table 'problems', force: :cascade do |t|
+  create_table 'math_trainer_problems', force: :cascade do |t|
     t.string 'type'
     t.integer 'number_1'
     t.integer 'number_2'
@@ -94,7 +173,10 @@ ActiveRecord::Schema[7.0].define(version: 20_231_203_230_226) do
     t.datetime 'updated_at', null: false
   end
 
-  create_table 'time_sessions', force: :cascade do |t|
+  create_table 'math_trainer_schema_migrations', primary_key: 'version', id: :string, force: :cascade do |t|
+  end
+
+  create_table 'math_trainer_time_sessions', force: :cascade do |t|
     t.bigint 'user_id', null: false
     t.integer 'minutes'
     t.datetime 'created_at', null: false
@@ -104,7 +186,7 @@ ActiveRecord::Schema[7.0].define(version: 20_231_203_230_226) do
     t.index ['user_id'], name: 'index_math_trainer_time_sessions_on_user_id'
   end
 
-  create_table 'users', force: :cascade do |t|
+  create_table 'math_trainer_users', force: :cascade do |t|
     t.string 'DeviseCreateUsers'
     t.string 'email', default: ''
     t.string 'encrypted_password', default: '', null: false
@@ -128,6 +210,49 @@ ActiveRecord::Schema[7.0].define(version: 20_231_203_230_226) do
     t.index ['uuid'], name: 'index_math_trainer_users_on_uuid', unique: true
   end
 
+  create_table 'problems', force: :cascade do |t|
+    t.string 'type'
+    t.integer 'number_1'
+    t.integer 'number_2'
+    t.integer 'hole_position'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+  end
+
+  create_table 'time_sessions', force: :cascade do |t|
+    t.bigint 'user_id', null: false
+    t.integer 'minutes'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.jsonb 'operation_types', default: {}
+    t.boolean 'shuffle_hole_position', default: false
+    t.index ['user_id'], name: 'index_time_sessions_on_user_id'
+  end
+
+  create_table 'users', force: :cascade do |t|
+    t.string 'DeviseCreateUsers'
+    t.string 'email', default: ''
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at', precision: nil
+    t.datetime 'remember_created_at', precision: nil
+    t.integer 'sign_in_count', default: 0, null: false
+    t.datetime 'current_sign_in_at', precision: nil
+    t.datetime 'last_sign_in_at', precision: nil
+    t.inet 'current_sign_in_ip'
+    t.inet 'last_sign_in_ip'
+    t.string 'confirmation_token'
+    t.datetime 'confirmed_at', precision: nil
+    t.datetime 'confirmation_sent_at', precision: nil
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.uuid 'uuid'
+    t.index ['confirmation_token'], name: 'index_users_on_confirmation_token', unique: true
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+    t.index ['uuid'], name: 'index_users_on_uuid', unique: true
+  end
+
   add_foreign_key 'answer_fights', 'answers'
   add_foreign_key 'answer_fights', 'fights'
   add_foreign_key 'answers', 'card_sessions'
@@ -140,5 +265,18 @@ ActiveRecord::Schema[7.0].define(version: 20_231_203_230_226) do
   add_foreign_key 'card_sessions', 'users'
   add_foreign_key 'fights', 'fight_opponents'
   add_foreign_key 'fights', 'users'
+  add_foreign_key 'math_trainer_answer_fights', 'math_trainer_answers', column: 'answer_id'
+  add_foreign_key 'math_trainer_answer_fights', 'math_trainer_fights', column: 'fight_id'
+  add_foreign_key 'math_trainer_answers', 'math_trainer_card_sessions', column: 'card_session_id'
+  add_foreign_key 'math_trainer_answers', 'math_trainer_problems', column: 'problem_id'
+  add_foreign_key 'math_trainer_answers', 'math_trainer_users', column: 'user_id'
+  add_foreign_key 'math_trainer_answers_sessions', 'math_trainer_answers', column: 'answer_id'
+  add_foreign_key 'math_trainer_answers_sessions', 'math_trainer_time_sessions', column: 'time_session_id'
+  add_foreign_key 'math_trainer_card_session_problems', 'math_trainer_card_sessions', column: 'card_session_id'
+  add_foreign_key 'math_trainer_card_session_problems', 'math_trainer_problems', column: 'problem_id'
+  add_foreign_key 'math_trainer_card_sessions', 'math_trainer_users', column: 'user_id'
+  add_foreign_key 'math_trainer_fights', 'math_trainer_fight_opponents', column: 'fight_opponent_id'
+  add_foreign_key 'math_trainer_fights', 'math_trainer_users', column: 'user_id'
+  add_foreign_key 'math_trainer_time_sessions', 'math_trainer_users', column: 'user_id'
   add_foreign_key 'time_sessions', 'users'
 end
